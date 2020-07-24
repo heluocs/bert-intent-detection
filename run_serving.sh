ef17cfca7c6161f698663cfd6d139ff27eb554af
heluocs <yukong.lxx@alibaba-inc.com>
2020-07-24 11:21:05 +0800

    delete script

--- run_serving.sh
+++
@@ -1,3 +0,0 @@
-docker run -p 8501:8501 \
-    --mount type=bind,source=/root/workspace/bert/SavedModel/,target=/models/uncased_L-12_H-768_A-12 \
-    -e MODEL_NAME=uncased_L-12_H-768_A-12 -t tensorflow/serving
